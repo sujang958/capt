f2c45c914b3f97507b1676501d6cbfb48dee3403
sujang958 <sujangdev@gmail.com>
2025-04-12 20:43:45 +0900

    conf: add tools

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { onMount } from "svelte"
+import "./app.css"
+import { invoke } from "@tauri-apps/api/core"
+
+onMount(() => {
+	const supportedConstraints = navigator.mediaDevices.getSupportedConstraints()
+	const devices = navigator.mediaDevices.enumerateDevices() // Finally, i found the sauce 😭
+	console.log(devices.then(console.log))
+	const stream = navigator.mediaDevices.getUserMedia({
+		audio: { deviceId: "" },
+	})
+})
+</script>
+
+<main>
+  <p class="text-3xl">dafsfsfd</p></main>
