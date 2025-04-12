d35b4d15dfe2cd305bf656c918df3c3f274654d5
sujang958 <sujangdev@gmail.com>
2025-04-13 02:12:47 +0900

    feat: construst a layout

---
+++ src/components/AudioTrack.svelte
@@ -0,0 +1,8 @@
+<div>
+  <p class="text-sm text-neutral-200">Track 1</p>
+  <div class="flex flex-row items-center justify-between mt-2">
+    <select name="" id="" class="bg-neutral-700 rounded-lg px-2 py-1.5">
+      <option value="asdfasf">asdfasfdsa</option>
+    </select>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
-import { onMount } from "svelte"
-import "./app.css"
-import { invoke } from "@tauri-apps/api/core"
+  import { onMount } from "svelte"
+  import "./app.css"
+  import { invoke } from "@tauri-apps/api/core"
+  import AudioTrack from "../components/AudioTrack.svelte"
 
-onMount(() => {
-	const supportedConstraints = navigator.mediaDevices.getSupportedConstraints()
-	const devices = navigator.mediaDevices.enumerateDevices() // Finally, i found the sauce 😭
-	console.log(devices.then(console.log))
-	const stream = navigator.mediaDevices.getUserMedia({
-		audio: { deviceId: "" },
-	})
-})
+  onMount(() => {
+    const supportedConstraints =
+      navigator.mediaDevices.getSupportedConstraints()
+    const devices = navigator.mediaDevices.enumerateDevices() // Finally, i found the sauce 😭
+    console.log(devices.then(console.log))
+    const stream = navigator.mediaDevices.getUserMedia({
+      audio: { deviceId: "" },
+    })
+  })
 </script>
 
-<main>
-  <p class="text-3xl">dafsfsfd</p></main>
+<main class="w-full h-full px-6 py-3 flex flex-col gap-y-4">
+  <section>
+    <p class="text-sm font-medium">Video</p>
+    <img src="/asdf.png" alt="" class="mt-2" />
+  </section>
+
+  <section class="flex flex-row items-start gap-x-6">
+    <div class="flex-1">
+      <p class="text-sm font-medium">Audio Trakcs</p>
+      <div class="bg-neutral-800 rounded-lg p-6 mt-2 flex flex-col gap-y-4">
+        <AudioTrack />
+      </div>
+    </div>
+    <aside class="min-w-48">
+      <p class="text-sm font-medium">Controls</p>
+    </aside>
+  </section>
+</main>
