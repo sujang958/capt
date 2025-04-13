51fe54aa720d789fad4abfc0642f298d039a4a75
sujang958 <sujangdev@gmail.com>
2025-04-13 15:41:27 +0900

    feat: create AudioTrack component

--- src/components/AudioTrack.svelte
+++ src/components/AudioTrack.svelte
@@ -1,21 +1,26 @@
 <script>
   import { Trash2, Volume2 } from "lucide-svelte"
+  import Button from "./ui/Button.svelte"
 </script>
 
 <div>
   <p class="text-sm text-neutral-200">Track 1</p>
   <div class="mt-2 flex flex-row items-center justify-between gap-x-12">
-    <select name="" id="" class="w-full rounded-lg bg-neutral-700 px-2 py-1">
-      <option value="asdfasf">asdfasfdsa</option>
+    <select
+      name=""
+      id=""
+      class="form-select w-full rounded-lg bg-neutral-700 px-2 py-1"
+    >
+      <option value="asdfasf" class="">asdfasfdsa</option>
     </select>
     <div class="flex flex-row items-center gap-x-3">
       <!-- svelte-ignore a11y_consider_explicit_label -->
-      <button class="cursor-pointer rounded-lg bg-neutral-700 p-2" type="button"
-        ><Volume2 class="size-4" />
-      </button>
-      <button class="cursor-pointer rounded-lg bg-red-300 p-2" type="button"
-        ><Trash2 class="size-4 stroke-red-900" />
-      </button>
+      <Button class="p-2">
+        <Volume2 class="size-4" />
+      </Button>
+      <Button class="bg-red-300 p-2">
+        <Trash2 class="size-4 stroke-red-900" />
+      </Button>
     </div>
   </div>
 </div>
---
+++ src/components/ui/Button.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Button } from "bits-ui"
+  import { twMerge } from "tailwind-merge"
+
+  let { class: className = "", children } = $props()
+</script>
+
+<Button.Root
+  class={twMerge(
+    "cursor-pointer rounded-lg bg-neutral-700 px-3 py-1 text-white transition duration-200 hover:opacity-80",
+    className,
+  )}
+>
+  {@render children()}
+</Button.Root>
